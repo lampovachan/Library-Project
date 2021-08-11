ff999af7af52357a1aeaa823eecccebbf475b203
Svitlana Tkachuk <misstkachuk15@gmail.com>
2021-08-11 14:57:59 +0300

    add swagger

---
+++ src/main/java/com/tkachuk/library/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.tkachuk.library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+
+@Configuration
+@Import(springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration.class)
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .securitySchemes(Arrays.asList(apiKey()));
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Sig-Predict REST API Document")
+                .description("work in progress")
+                .termsOfServiceUrl("localhost")
+                .version("1.0")
+                .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("jwtToken", "Authorization", "header");
+    }
+}
